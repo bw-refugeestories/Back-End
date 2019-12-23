2e0e2f3329d76c70e40b4b958cc7950f6cd9840c
Nick <hansen.nick95@gmail.com>
2019-12-23 00:14:05 -0800

    Hopefully fixed heroku crashing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
   console.log(`**  Server listening on port ${port} **`)
