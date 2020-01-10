4638f37140a1d43dd8ddef7da55fc4b8bbb84429
Nick <hansen.nick95@gmail.com>
2020-01-09 19:59:02 -0800

    added newID

--- stories/acceptedStories.js
+++ stories/acceptedStories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const helpers = require('./acceptedStoryHelpers');
+const protected = require('../middleware/restricted-middleware');
 
 router.get('/', (req, res) => {
   helpers.find()
@@ -17,6 +18,18 @@ router.get('/:id', (req, res) => {
   helpers.findByID(id)
     .then(story => res.status(200).json(story))
     .catch(err => res.status(500).json(err))
+})
+
+router.delete('/delete/:id', protected, (req, res) => {
+  const {id} = req.params;
+
+  helpers.deleteByID(id)
+    .then( result => {
+      res.status(200).json({message: "deleted!"})
+    })
+    .catch( err => {
+      res.status(500).json({message: 'There is a problem with the server'})
+    })
 })
 
 module.exports = router
